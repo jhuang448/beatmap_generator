ca05e1f8f8096e3430c1d4acf63ca13cb23b1f59
Yihao Chen <ychen3237@gatech.edu>
2019-12-02 18:38:44 -0500

    Automate Generation

---
+++ script_win.bat
@@ -0,0 +1,7 @@
+:: Modify name fields before running
+python map_reader.py [Osu File Path] [ffmpeg Path]
+pause
+python rhythm_evaluator.py [Osu File Path] [Model_1] [Model_2] (Optional: Mapthis Npz File)
+pause
+python GAN.py rhythm_data.npz flow_dataset.npz
+pause
